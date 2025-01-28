4998a3eb34a18f0754be1d80b4f62a14374f06d1
rebpacquiao <reb.pacquiao@gmail.com>
2025-01-28 17:15:15 +0800

    Add search functionality to RecipesPage; implement loading state and update styling

--- services/recipes.ts
+++ services/recipes.ts
@@ -3,3 +3,11 @@ export async function getRecipes() {
   const data = await response.json();
   return data;
 }
+
+export async function searchRecipes(query: string) {
+  const response = await fetch(
+    `https://dummyjson.com/recipes/search?q=${query}`
+  );
+  const data = await response.json();
+  return data;
+}
